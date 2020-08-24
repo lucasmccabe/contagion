dd402ed56e0d4ad91e9f925791ac6294be0dfbfe
Lucas <lucashmccabe@gmail.com>
2020-08-24 19:39:44 -0400

    Updated configuration file.

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ extensions = [
     'numpydoc']
 numpydoc_show_class_members = False
 
-.. automodapi:: mypackage.mymodule
+#.. automodapi:: mypackage.mymodule
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
